1099c42c0d76c499a796fc9aa33f8841d559b96b
KritoAsuna <zhchx1996@outlook.com>
2018-05-09 20:25:50 +0800

    Update main.c

--- main.c
+++ main.c
@@ -8,11 +8,11 @@
 int main()
 {
 	double *A, *originA, *tmp; /* matrix */
-	double *y, *b, *originb; /* vectors */
-	double start, end;
-	int r, c;
+	double *x, *y, *b, *originb; /* vectors */
+	double start, end; //time conuter
+	int r, c; // row and column 
 	int n; //number of equations
-	int thread_count;
+	int thread_count; 
 
 	printf("Please input the number of equations:");
 	scanf("%d", &n);
@@ -20,23 +20,27 @@ int main()
 	r = n;
 	c = n;
 
-	A = (double*)malloc(r*c*sizeof(double));
+	A = (double*)malloc(r*c*sizeof(double));	//memory for Matrix
 	if (A == NULL)
 		return -1;
 
-	originA = (double*)malloc(r*c*sizeof(double));
+	originA = (double*)malloc(r*c*sizeof(double)); //memory for original Matrix
 	if (originA == NULL)
 		return -1;
 
-	b = (double*)malloc(r*sizeof(double));
+	b = (double*)malloc(r*sizeof(double)); //memory for vector
 	if (b == NULL)
 		return -1;
 
-	originb = (double*)malloc(r*sizeof(double));
+	originb = (double*)malloc(r*sizeof(double)); //memory for original vector
 	if (originb == NULL)
 		return -1;
 
-	y = (double*)malloc(r*sizeof(double));
+	x = (double *)malloc(r * sizeof(double));
+	if (x == NULL)
+		return -1;
+
+	y = (double*)malloc(r*sizeof(double)); //memory for substitution vector
 	if (y == NULL)
 		return -1;
 
@@ -48,17 +52,17 @@ int main()
 	originA = A;
 	originb = b;
 
+	//inintialize matrix and vector
 	Mat_Init(r, c, A);
 	Vec_Init(r, b);
 
-	printf("\n Original Matrix: ");
+	printf("\n-----------------------------------------------\nOriginal Matrix: ");
 	Mat_Show( r,  c,  A);
-	printf("\n Original vectors: ");
+	printf("\n-----------------------------------------------\nOriginal vectors:");
 	Vec_Show(n , b);
-	printf("\n After elimination: ");
-	gauss_elimination(A, n, b, y);
-	Mat_Show(n, n, A);
-	Vec_Show(n, b);
+	printf("\n----------------After elimination------------------\n");
+	gauss_elimination(A, n, b, x, y);
+	
 	/* calculate Xv and record its running time */
 	start = omp_get_wtime();
 
