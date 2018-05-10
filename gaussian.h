1e981a1bddb5973dc5590297ddfe0703b1a91579
KritoAsuna <zhchx1996@outlook.com>
2018-05-10 13:57:23 +0800

    add gauss_elimination_omp function

--- gaussian.h
+++ gaussian.h
@@ -3,8 +3,6 @@
 #ifndef GAUSSIAN_H
 #define GAUSSIAN_H
 
-
-// =============================================================================
 /* initialize a matrix x */
 void Mat_Init(int row, int col, double *X);
 
@@ -17,17 +15,10 @@ void Vec_Init(int size, double *x);
 /* display a vector */
 void Vec_Show(int size, double *b);
 
-// =============================================================================
-double *calculate_solution(double **m, int r);
-// =============================================================================
-void fill_matrix(double **m, int r, int c, char *file);
-// =============================================================================
-void print_matrix(double **m, int r, int c, char *file);
-// =============================================================================
 void free_matrix(double **m, int r, int c);
-// =============================================================================
-void gauss_elimination(double *A, int n, double *b, double *x, double *y);
-// =============================================================================
 
+void gauss_elimination(double *A, int n, double *b, double *x, double *y);
+
+void gauss_elimination_omp(double *A, int n, double *b, double *x, double *y, int thread_count);
 
 #endif
