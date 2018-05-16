14245d123acd15115b039492d592690d0928c718
KritoAsuna <zhchx1996@outlook.com>
2018-05-16 11:53:17 +0800

    Update main.c

--- main.c
+++ main.c
@@ -7,15 +7,16 @@
 #include <omp.h>
 #include "gaussian.h"
 
+
 int main()
 {
 	double *originA, *A, *pA;  // matrix   A for single thread; pA for OpenMP
 	double *x, *y, *xnew, *ynew;   // vectors x,y for single thread; xnew, ynew for OpenMP
-	double *b, *pb, *originb, *r1, *r2;  // vectors  b for single thread; pb for OpenMP
-	double start, end, start1, end1; //time conuter
+	double *b, *pb, *originb;  // vectors  b for single thread; pb for OpenMP
+	double start, end; //time conuter
 	int r, c; // row and column 
 	int n; //number of equations
-	int thread_count;
+	int thread_count, correct_count, correct_count_omp;
 	int result;
 
 	printf("Please input the number of equations:");
@@ -24,28 +25,30 @@ int main()
 	r = n;
 	c = n;
 
-	A = (double*)malloc(r*c*sizeof(double));	//memory for Matrix
-	if (A == NULL)
-		return -1;
-
 	originA = (double*)malloc(r * c * sizeof(double)); //memory for original Matrix
 	if (originA == NULL)
+		return -1;
+
+	A = (double*)malloc(r*c*sizeof(double));	//memory for Matrix
+	if (A == NULL)
 		return -1;
 
 	pA = (double*)malloc(r*c*sizeof(double));	//memory for Matrix
 	if (pA == NULL)
 		return -1;
 
-	b = (double*)malloc(r * sizeof(double)); //memory for vector
-	if (b == NULL)
-		return -1;
-	pb = (double*)malloc(r * sizeof(double)); //memory for vector
-	if (pb == NULL)
-		return -1;
 
 	originb = (double*)malloc(r * sizeof(double)); //memory for original vector
 	if (originb == NULL)
 		return -1;
+
+	b = (double*)malloc(r * sizeof(double)); //memory for vector
+	if (b == NULL)
+		return -1;
+	
+	pb = (double*)malloc(r * sizeof(double)); //memory for vector
+	if (pb == NULL)
+		return -1;	
 
 
 
@@ -66,28 +69,12 @@ int main()
 		return -1;
 
 
-	r1 = (double *)malloc(r * sizeof(double)); //memory for result of single thread calculation
-	if (r1 == NULL)
-		return -1;
-
-	r2 = (double *)malloc(r * sizeof(double)); //memory for result OpenMp calculation
-	if (r1 == NULL)
-		return -1;
-
 	/* Initialize the matrix X and vector v*/
 	srand((unsigned)time(NULL));
 
 	//inintialize matrix and vector
 	Mat_Init(r, c, originA);
 	Vec_Init(r, originb);
-
-	for (int i = 0; i < n; i++)
-	{
-		x[i] = 0;
-		y[i] = 0;
-		xnew[i] = 0;
-		ynew[i] = 0;
-	}
 
 	//copy matrix from A to originA and pA;
 	for (int i = 0; i < n * n; i++)
@@ -102,10 +89,10 @@ int main()
 		pb[i] = originb[i];
 	}
 
-	//printf("\noriginal Matrix:");
-	//Mat_Show(n, n, originA);
-	//Mat_Show(n, n, pA);
-	//Vec_Show(n, originb);
+	printf("\noriginal Matrix:");
+//	Mat_Show(n, n, originA);
+//  Vec_Show(n, originb);
+
 
 	printf("\n-----------------------------------------------\nCalculation implements the single thread: ");
 	//printf("\n--After elimination------------------\n");
@@ -113,13 +100,14 @@ int main()
 	start = omp_get_wtime();
 	gauss_elimination(A, n, b, x, y);
 	end = omp_get_wtime();
+	
 	printf("Single thread takes %f millisecond.\n", (end - start) * 1000);
 
 	//do verification
-	int correctRNo = verification(x , A, b, n );
-	printf("%d ", correctRNo);
+	correct_count = verification(x, originA, originb, n);
+	printf("%d ", correct_count);
 	printf("The single thread calculation result is: ");
-	if (correctRNo == n)
+	if (correct_count == n)
 	{
 		printf("right\n!");
 	}
@@ -132,7 +120,6 @@ int main()
 
 	//using openMP 	
 	printf("\n-----------------------------------------------\nCalculation implements the OpenMP: ");
-	printf("\nIt will loop 10 times to test withdiffernet thread counts");
 	
 		//try do many times for one set of equations
 		printf("\nPlease input the number of threads: ");
@@ -140,15 +127,15 @@ int main()
 		/* calculate and record its running time */
 		start = omp_get_wtime();
 		gauss_elimination_omp(pA, n, pb, xnew, ynew, thread_count);
-		
-
 		end = omp_get_wtime();
+		
+		
 		printf("The calculation cost %f milliseconds with openmp methord\n", (end - start) * 1000);
 				//do verification
-//		int correct = verification(xnew, pA, pb, n);
-		int correct = verification(xnew, A, b, n);
-		printf("%d ", correctRNo);
-		if (correctRNo == n)
+		correct_count_omp = verification(xnew, originA, originb, n);
+		printf("%d ", correct_count_omp);
+		printf("The multi-thread calculation result is: ");
+		if (correct_count_omp == n)
 		{
 			printf("The result is right\n!");
 		}
@@ -170,7 +157,7 @@ int main()
 }
 
 //function fo verifycation of the result
-int verification(double *x, double *A, double *b, int n)
+int verification(double *x, double *A, double *originb, int n)
 {
 	double *verifyB;
 	int correctRNo = 0;
@@ -178,11 +165,6 @@ int verification(double *x, double *A, double *b, int n)
 	verifyB = (double *)malloc(n * sizeof(double));
 	if (verifyB == NULL)
 		return -1;
-
-	for (int i = 0; i < n; i++)
-	{
-		verifyB[i] = 0;
-	}
 
 	//caculate the verifid b with the calculated result of x[i]
 	for (int i = 0; i < n; i++)
@@ -196,12 +178,14 @@ int verification(double *x, double *A, double *b, int n)
 	//increase the correct NO. count  
 	for (int i = 0; i < n; i++)
 	{
-		if (fabs(verifyB[i] - b[i]) <= 0.0001)
+		if (fabs(verifyB[i] - originb[i]) <= 0.000001)
 		{
 			correctRNo = correctRNo + 1;
+			printf("i = %d: %f %f\n", i, verifyB[i], originb[i]);
 		}
-		else
-			printf("i = %d: %f %f\n", i, verifyB[i], b[i]);
+		else{
+			printf("i = %d: %f %f\n", i, verifyB[i], originb[i]);
+		}
 	}
 
 	free(verifyB);
